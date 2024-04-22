e53269b2e3a3a20e946cdba94bc97ec69a82be8e
Anso <anso@gmail.com>
2024-04-22 11:32:22 +0200

    Definition of the Todo Class

--- todoListManager.js
+++ todoListManager.js
@@ -3,3 +3,14 @@
 // Supprimer une tâche existante.
 // Marquer une tâche comme complétée.
 // Sauvegarder l'état actuel des tâches (dans le stockage local du navigateur ou sur un serveur distant). Pensez à génerer une id unique
+
+
+class Todo {
+
+  constructor(id= crypto.randomUUID(), content, status, createdAt = new Date()) {
+    this.id = id,
+    this.content = content,
+    this.status = status,
+    this.createdAt = createdAt;
+  }
+}
